c2936743764fbb080fc5cfade18f92f6d5e059ff
Kathy <kleboeuf@townisp.com>
2018-05-18 23:41:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function appendKittens(array, element) {
 }
 
 //console.log(kittens);
-//console.log(appendKittens(kittens, "Snowball"));
+console.log(appendKittens(kittens, "Snowball"));
 
 //pop(removes last and returns that element)
 //push(adds to end and returns array new length)
